b98a94d87a261b178c72799868769895be263d0d
Chisom Oguine <oguinechisomtimothy@gmail.com>
2022-06-21 01:03:43 -0700

    modified all files

---
+++ index.js
@@ -0,0 +1,7 @@
+const myName = document.querySelector(".myName")
+const height = document.querySelector(".height")
+const country = document.querySelector(".country")
+
+myName.innerHTML = "Name: Chisom Timothy Oguine"
+height.innerHTML = "Height: 5ft8"
+country.innerHTML = "Country: Nigeria"
